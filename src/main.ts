dcb42c77212714c665f7e2015ff14eb1d7fe86c4
Cynthia Tedesco <cynthia.tedesco@gmail.com>
2024-10-06 17:22:30 +0200

    Reset

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,5 @@
 import { createApp } from 'vue'
+import './style.css'
 import App from './App.vue'
-import router from './router'
-import { createPinia } from "pinia"
-import "./assets/css/app.scss"
 
-createApp(App).use(router).use(createPinia()).mount('#app')
+createApp(App).mount('#app')
